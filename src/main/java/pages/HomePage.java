d0bcdc13dbf8aae79572f7d0936d90837ffd0a57
Artsem Filatau <filatau@adani.by>
2021-03-29 00:02:41 +0300

    Test for iPhone was added

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,11 +2,18 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
     private WebDriver driver;
 
     private By currencyLink = By.cssSelector("#currency-informer > a");
+    private By navigationText = By.cssSelector(".b-main-navigation__text");
+    private By quickSearch = By.cssSelector(".fast-search__form > .fast-search__input");
+
+    private By resultItems = By.cssSelector("div.result__item");
+    private By productDetails = By.cssSelector(".product__details > .product__title");
+
 
     public HomePage(WebDriver driver){
         this.driver = driver;
@@ -17,7 +24,25 @@ public class HomePage {
         return new CurrencyPage(driver);
     }
 
+    // NEED CHECK!!!
+    public String getCatalogMenu(int item){
+        return driver.findElements(navigationText).get(item).getText();
+    }
 
+    public void searchProduct(String product){
+        driver.findElement(quickSearch).sendKeys(product);
+    }
+
+    public ProductPage selectFirstProduct(){
+        WebElement item = driver.findElements(resultItems).get(0);
+        item.findElement(productDetails).click();
+        return new ProductPage(driver);
+    }
+
+    public ProductPage testMethod_del_it(){
+        driver.get("https://catalog.onliner.by/mobile/apple/iphone7pr128");
+        return new ProductPage(driver);
+    }
 
 
 }
