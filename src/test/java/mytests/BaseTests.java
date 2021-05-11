f36a8fa37b56f16e4e2c51838f6683649ea39f10
Artsem Filatau <filatau@adani.by>
2021-05-11 22:56:49 +0300

    logger plus allure(not success)

--- src/test/java/mytests/BaseTests.java
+++ src/test/java/mytests/BaseTests.java
@@ -3,6 +3,8 @@ package mytests;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pages.HomePage;
 
 import java.util.concurrent.TimeUnit;
@@ -12,6 +14,8 @@ public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
     private String URL = "http://onliner.by";
+    final Logger logger = LoggerFactory.getLogger(BaseTests.class);
+
 
     @BeforeAll
     public void setup(){
@@ -19,10 +23,9 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get(URL);
         driver.manage().window().maximize();
+        logger.info("Full screen");
         homePage = new HomePage(driver);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-//        List<WebElement> links = driver.findElements(By.tagName("a"));
-//        System.out.println(links.size());
+        logger.debug("Browser is opened");
     }
 }
